c32122c8ff54056b3673cf6880d55bc757b4baf8
AlejandroLHC <alejandrolhc@gmail.com>
2022-07-21 14:46:30 +0200

    Cleaning JS file

--- app.js
+++ app.js
@@ -1,57 +1,65 @@
 
 	// Capturing buttons
+
 const
-	one 			= document.getElementById('one'),
-	two 			= document.getElementById('two'),
-	three 			= document.getElementById('three'),
-	four 			= document.getElementById('four'),
-	five 			= document.getElementById('five'),
-	six 			= document.getElementById('six'),
-	seven 			= document.getElementById('seven'),
-	eight 			= document.getElementById('eight'),
-	nine 			= document.getElementById('nine'),
-	zero			= document.getElementById('zero'),
-	add 			= document.getElementById('add'),
-	subtract		= document.getElementById("subtract"),
-	multiply 		= document.getElementById("multiply"),
-	divide 			= document.getElementById("divide"),
-	power 			= document.getElementById("pwr"),
-	root            = document.getElementById('root'),
-	percent         = document.getElementById('percent'),
-	reverse 		= document.getElementById("reverse"),
-	clean 			= document.getElementById("clean"),
-	allClear		= document.getElementById("ac"),
-	equals 			= document.getElementById("equals")
-;
+one 			= document.getElementById('one'),
+two 			= document.getElementById('two'),
+three 			= document.getElementById('three'),
+four 			= document.getElementById('four'),
+five 			= document.getElementById('five'),
+six 			= document.getElementById('six'),
+seven 			= document.getElementById('seven'),
+eight 			= document.getElementById('eight'),
+nine 			= document.getElementById('nine'),
+zero			= document.getElementById('zero'),
+add 			= document.getElementById('add'),
+subtract		= document.getElementById("subtract"),
+multiply 		= document.getElementById("multiply"),
+divide 			= document.getElementById("divide"),
+power 			= document.getElementById("pwr"),
+root            = document.getElementById('root'),
+percent         = document.getElementById('percent'),
+reverse 		= document.getElementById("reverse"),
+clean 			= document.getElementById("clean"),
+allClear		= document.getElementById("ac"),
+equals 			= document.getElementById("equals");
 
 	// Capturing the display
+
 const 
-    result 		= document.getElementById("result");
-    preview 	= document.getElementById("preview");
-;
-
-	// Events input numbers
-
-	zero.onclick                =()=> result.textContent += "0";
-	one.onclick                 =()=> result.textContent += "1";
-	two.onclick                 =()=> result.textContent += "2";
-	three.onclick               =()=> result.textContent += "3";
-	four.onclick                =()=> result.textContent += "4";
-	five.onclick                =()=> result.textContent += "5";
-	six.onclick                 =()=> result.textContent += "6";
-	seven.onclick               =()=> result.textContent += '7';
-	eight.onclick               =()=> result.textContent += "8";
-	nine.onclick                =()=> result.textContent += "9";
-	float.onclick               =()=> result.textContent += ".";
-	percent.onclick             =()=> result.textContent = parseFloat(result.textContent) / 100;  
-	clean.onclick				=()=> clear();	
-	allClear.onclick            =()=> clearAll();
+result 			= document.getElementById("result"),
+preview 		= document.getElementById("preview");
 
 
-	//auxiliar variables
-var operandA;
-var operandB;
-var operator = "";
+	// Events number input
+
+zero.onclick                =()=> result.textContent += "0";
+one.onclick                 =()=> result.textContent += "1";
+two.onclick                 =()=> result.textContent += "2";
+three.onclick               =()=> result.textContent += "3";
+four.onclick                =()=> result.textContent += "4";
+five.onclick                =()=> result.textContent += "5";
+six.onclick                 =()=> result.textContent += "6";
+seven.onclick               =()=> result.textContent += '7';
+eight.onclick               =()=> result.textContent += "8";
+nine.onclick                =()=> result.textContent += "9";
+float.onclick               =()=> result.textContent += ".";
+percent.onclick             =()=> result.textContent = parseFloat(result.textContent) / 100;  
+clean.onclick				=()=> clear();	
+allClear.onclick            =()=> clearAll();
+
+
+
+
+	// Events operation input
+
+reverse.onclick = function() {
+	if (result.textContent[0] !== "-") {
+		result.textContent = `-${result.textContent}`;
+	} else {
+		result.textContent = result.textContent.substring(1);
+	}
+}
 
 add.onclick = function() {
 	if (operator !== "") {
@@ -124,26 +132,36 @@ equals.onclick = function(e) {
 		operandB = result.textContent;
 		preview.textContent += operandB + ")=";
 		solve();
-	 } else if (operator !== "") {
+	} else if (operator !== "") {
 		operandB = result.textContent;
 		preview.textContent += operandB + "=";
 		solve();
 	}
 }
 
+	// Auxiliar variables
+
+var
+operandA,
+operandB,
+operator = "";
+
+	// Functions 
+
 function clearAll() {
-    preview.textContent = "";
+	preview.textContent = "";
     result.textContent = "";
     operandA = 0;
     operandB = 0;
     operator = "";
 }
+
 function clear() {
-    result.textContent = "";
+	result.textContent = "";
 }
 
 function clearSolve() {
-    result.textContent = "";
+	result.textContent = "";
     operandA = 0;
     operandB = 0;
     operator = "";
